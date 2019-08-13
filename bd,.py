be42e69729d18ae831a5630b59f2b7a82b552336
shewitt.au <shewitt.au@gmail.com>
2019-08-14 01:45:40 +1000

    More

--- bd,.py
+++ bd,.py
@@ -5,6 +5,21 @@ from math import log
 f0 = 440
 a = 2**(1/12)
 
+# http://www.cbmitapages.it/c64/sid1eng.htm
+# https://codebase64.org/doku.php?id=base:how_to_calculate_your_own_sid_frequency_table
+
+def reg_to_freq_pal(reg):
+	return (reg*985250)/16777216
+
+def freq_to_reg_pal(f):
+	return int((f*16777216)/985250)
+
+def reg_to_freq_ntsc(reg):
+	return (reg*1022730)/16777216
+
+def freq_to_reg_ntsc(f):
+	return int((f*16777216)/1022730)
+
 def note_to_freq(n):
 	return f0*a**n
 
@@ -12,5 +27,5 @@ def freq_to_note(f):
 	return log(f/f0, a)
 
 if __name__=='__main__':
-	print(freq_to_note(440))
-	print(freq_to_note(note_to_freq(2)))
+	print(hex(freq_to_reg_pal(440)))
+	print(hex(freq_to_reg_ntsc(440)))
